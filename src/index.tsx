b8838755001bcb6a13fcbe544237c0c351bfb45a
Vera <ahde.vera@icloud.com>
2024-01-01 16:08:16 +0200

    degrade react v - 17
    degrade react v - 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/styles.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import {ThemeProvider} from './shared/ThemeContext';
+import ReactDOM from 'react-dom';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+// const container = document.getElementById('root')!;
+// const root = createRoot(container);
 
-root.render(
+const rootNode = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <Provider store ={store}>
       <ThemeProvider>
         <App/>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+rootNode);
+
+
+// root.render(
+//   <React.StrictMode>
+//     <Provider store ={store}>
+//       <ThemeProvider>
+//         <App/>
+//       </ThemeProvider>
+//     </Provider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
